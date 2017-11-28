6f22efcb3431dca37879a9720d454cd3a11f69e4
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-11-28 11:52:11 +0800

    添加是否支持打印快递单模板配置项

--- config.php
+++ config.php
@@ -51,6 +51,7 @@ return array(
     'shipping_code' => 'ship_sf_express',
 	'insure' 		=> false,		/* 不支持保价 */
 	'cod' 			=> true, 		/* 配送方式是否支持货到付款 */
+    'print_support' => true,
 	'print_model'	=> 2,			/* 模式编辑器 */
 	'print_bg'		=> RC_Plugin::plugins_url('images/dly_sf_express.jpg',__FILE__) ,			/* 打印单背景 */
 	'config_lable'	=> 't_shop_name,' . RC_Lang::lang('lable_box/shop_name') . ',150,29,112,137,b_shop_name||,||t_shop_address,' . RC_Lang::lang('lable_box/shop_address') . ',268,55,105,168,b_shop_address||,||t_shop_tel,' . RC_Lang::lang('lable_box/shop_tel') . ',55,25,177,224,b_shop_tel||,||t_customer_name,' . RC_Lang::lang('lable_box/customer_name') . ',78,23,299,265,b_customer_name||,||t_customer_address,' . RC_Lang::lang('lable_box/customer_address') . ',271,94,104,293,b_customer_address||,||',			/* 打印快递单标签位置信息 */

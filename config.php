c149d064d0438ed46ad3f7b6d3e48de69071be2b
宋倩倩 <huangyuyuan@ecmoban.comshuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2019-03-22 15:17:42 +0800

    更新配送方式安装参数

--- config.php
+++ config.php
@@ -53,15 +53,28 @@ return array(
     'print_support' => true,
 	'print_model'	=> 2,			/* 模式编辑器 */
 	'print_bg'		=> 'images/dly_sf_express.jpg',			/* 打印单背景 */
-	'config_lable'	=> 't_shop_name,shop_name,150,29,112,137,b_shop_name||,||t_shop_address,shop_address,268,55,105,168,b_shop_address||,||t_shop_tel,shop_tel,55,25,177,224,b_shop_tel||,||t_customer_name,customer_name,78,23,299,265,b_customer_name||,||t_customer_address,customer_address,271,94,104,293,b_customer_address||,||',			/* 打印快递单标签位置信息 */
+// 	'config_lable'	=> 't_shop_name,shop_name,150,29,112,137,b_shop_name||,||t_shop_address,shop_address,268,55,105,168,b_shop_address||,||t_shop_tel,shop_tel,55,25,177,224,b_shop_tel||,||t_customer_name,customer_name,78,23,299,265,b_customer_name||,||t_customer_address,customer_address,271,94,104,293,b_customer_address||,||',			/* 打印快递单标签位置信息 */
+		
+	/**
+	 * 打印快递单标签位置信息
+	 * 使用 ||,|| 合并连接
+	 */
+	'config_lable'  => array(
+		't_shop_name,shop_name,150,29,112,137,b_shop_name',
+		't_shop_address,shop_address,268,55,105,168,b_shop_address',
+		't_shop_tel,shop_tel,55,25,177,224,b_shop_tel',
+		't_customer_name,customer_name,78,23,299,265,b_customer_name',
+		't_customer_address,customer_address,271,94,104,293,b_customer_address',
+		''
+	),
 		
 	'forms' => array(
-			array('name' => 'fee_compute_mode', 'type' => 'radiobox', 'value' => 'by_weight'), /* 费用计算方式  */
-			array('name' => 'item_fee',     'value' => 20),/* 单件商品的配送费用 */
-			array('name' => 'base_fee',    	'value' => 15), /* 1000克以内的价格   */
-			array('name' => 'step_fee',     'value' => 2),  /* 续重每1000克增加的价格 */
-			array('name' => 'free_money', 'type' => 'text', 'value' => 0), /* 免费额度  */
-			array('name' => 'pay_fee', 'type' => 'text', 'value' => 0), /* 货到付款支付费用  */
+		array('name' => 'fee_compute_mode', 'type' => 'radiobox', 'value' => 'by_weight'), /* 费用计算方式  */
+		array('name' => 'item_fee',     'value' => 20),/* 单件商品的配送费用 */
+		array('name' => 'base_fee',    	'value' => 15), /* 1000克以内的价格   */
+		array('name' => 'step_fee',     'value' => 2),  /* 续重每1000克增加的价格 */
+		array('name' => 'free_money', 'type' => 'text', 'value' => 0), /* 免费额度  */
+		array('name' => 'pay_fee', 'type' => 'text', 'value' => 0), /* 货到付款支付费用  */
 	),
 
     'dynamic_option' => array(
